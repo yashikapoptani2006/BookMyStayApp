fd8a3357f4272a17eec03e2eb113ce5a02b69e93
Yashika Poptani <poptaniyashika6@gmail.com>
2026-03-20 14:58:12 +0530

    UC2: Room types using abstraction and inheritance

---
+++ App/src/UseCase2RoomInitialization.java
@@ -0,0 +1,25 @@
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        SingleRoom single = new SingleRoom();
+        DoubleRoom dbl = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println("\nDouble Room:");
+        dbl.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println("\nSuite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
